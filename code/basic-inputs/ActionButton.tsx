b6cad2a982f88b638b12492faa3917d28873a1f6
iKettles <iain@framer.com>
2019-09-03 14:00:44 +0200

    First pass of DefaultButton

--- code/basic-inputs/ActionButton.tsx
+++ code/basic-inputs/ActionButton.tsx
@@ -3,83 +3,33 @@ import * as System from "office-ui-fabric-react";
 import { ControlType, PropertyControls, addPropertyControls } from "framer";
 import { withHOC } from "../utils/withHOC";
 
-const style: React.CSSProperties = {
-  width: "100%",
-  height: "100%"
-};
-
 const InnerActionButton: React.SFC = props => {
-  return <System.ActionButton {...props} style={style} />;
+  return (
+    <System.ActionButton {...props} iconProps={{ iconName: props.icon }} />
+  );
 };
 
 export const ActionButton = withHOC(InnerActionButton);
 
 ActionButton.defaultProps = {
   width: 150,
-  height: 50
+  height: 40
 };
 
 addPropertyControls(ActionButton, {
-  href: { title: "Href", defaultValue: "", type: ControlType.String },
-  primary: { title: "Primary", defaultValue: false, type: ControlType.Boolean },
-  uniqueId: { title: "UniqueId", defaultValue: "", type: ControlType.String },
+  text: {
+    title: "Text",
+    defaultValue: "Action Button",
+    type: ControlType.String
+  },
+  icon: {
+    title: "Icon",
+    type: ControlType.String,
+    defaultValue: "AddFriend"
+  },
   disabled: {
     title: "Disabled",
     defaultValue: false,
     type: ControlType.Boolean
-  },
-  allowDisabledFocus: {
-    title: "AllowDisabledFocus",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  primaryDisabled: {
-    title: "PrimaryDisabled",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  checked: { title: "Checked", defaultValue: false, type: ControlType.Boolean },
-  toggle: { title: "Toggle", defaultValue: false, type: ControlType.Boolean },
-  className: { title: "ClassName", defaultValue: "", type: ControlType.String },
-  ariaLabel: { title: "AriaLabel", defaultValue: "", type: ControlType.String },
-  ariaDescription: {
-    title: "AriaDescription",
-    defaultValue: "",
-    type: ControlType.String
-  },
-  ariaHidden: {
-    title: "AriaHidden",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  text: { title: "Text", defaultValue: "", type: ControlType.String },
-  split: { title: "Split", defaultValue: false, type: ControlType.Boolean },
-  splitButtonAriaLabel: {
-    title: "SplitButtonAriaLabel",
-    defaultValue: "",
-    type: ControlType.String
-  },
-  secondaryText: {
-    title: "SecondaryText",
-    defaultValue: "",
-    type: ControlType.String
-  },
-  buttonType: { title: "ButtonType", type: ControlType.Number },
-  toggled: { title: "Toggled", defaultValue: false, type: ControlType.Boolean },
-  menuTriggerKeyCode: { title: "MenuTriggerKeyCode", type: ControlType.Number },
-  persistMenu: {
-    title: "PersistMenu",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  description: {
-    title: "Description",
-    defaultValue: "",
-    type: ControlType.String
-  },
-  placeholder: {
-    title: "Placeholder",
-    defaultValue: "",
-    type: ControlType.String
   }
 });
