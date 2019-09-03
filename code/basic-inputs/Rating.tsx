17064d6b2cf2f51a23ada746f7c581d0d230d206
iKettles <iain@framer.com>
2019-09-03 17:29:01 +0200

    Implement Rating component

--- code/basic-inputs/Rating.tsx
+++ code/basic-inputs/Rating.tsx
@@ -2,14 +2,10 @@ import * as React from "react";
 import * as System from "office-ui-fabric-react";
 import { ControlType, PropertyControls, addPropertyControls } from "framer";
 import { withHOC } from "../utils/withHOC";
-
-const style: React.CSSProperties = {
-  width: "100%",
-  height: "100%"
-};
+import { RatingSize } from "office-ui-fabric-react";
 
 const InnerRating: React.SFC = props => {
-  return <System.Rating {...props} style={style} />;
+  return <System.Rating {...props} />;
 };
 
 export const Rating = withHOC(InnerRating);
@@ -19,10 +15,24 @@ Rating.defaultProps = {
   height: 50
 };
 
+const ratingKeys = Object.keys(RatingSize).filter((key: string | number) =>
+  isNaN(key as any)
+);
+
 addPropertyControls(Rating, {
-  rating: { title: "Rating", type: ControlType.Number },
-  min: { title: "Min", type: ControlType.Number },
-  max: { title: "Max", type: ControlType.Number },
+  rating: {
+    title: "Rating",
+    type: ControlType.Number,
+    defaultValue: 1,
+    min: 0,
+    displayStepper: true
+  },
+  max: {
+    title: "Max",
+    type: ControlType.Number,
+    defaultValue: 5,
+    displayStepper: true
+  },
   allowZeroStars: {
     title: "AllowZeroStars",
     defaultValue: false,
@@ -34,31 +44,21 @@ addPropertyControls(Rating, {
     defaultValue: "",
     type: ControlType.String
   },
-  size: { title: "Size", type: ControlType.Number },
-  ariaLabelFormat: {
-    title: "AriaLabelFormat",
-    defaultValue: "",
-    type: ControlType.String
-  },
-  ariaLabelId: {
-    title: "AriaLabelId",
-    defaultValue: "",
-    type: ControlType.String
+  size: {
+    title: "Size",
+    type: ControlType.SegmentedEnum,
+    defaultValue: String(RatingSize[ratingKeys[0]]),
+    options: ratingKeys.map(key => String(RatingSize[key])),
+    optionTitles: ratingKeys
   },
   readOnly: {
     title: "ReadOnly",
     defaultValue: false,
     type: ControlType.Boolean
   },
-  checked: { title: "Checked", defaultValue: false, type: ControlType.Boolean },
   disabled: {
     title: "Disabled",
     defaultValue: false,
     type: ControlType.Boolean
-  },
-  placeholder: {
-    title: "Placeholder",
-    defaultValue: "",
-    type: ControlType.String
   }
 });
