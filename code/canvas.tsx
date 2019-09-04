1ca07cf295310801b0582353ae7db99390665b09
Fernando Hurtado <fernandohur@gmail.com>
2019-09-04 11:24:34 +0200

    Added a few shared colors to canvas

--- code/canvas.tsx
+++ code/canvas.tsx
@@ -1,3 +1,10 @@
 // WARNING: this file is auto generated, any changes will be lost
 import { createDesignComponent, CanvasStore } from "framer"
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
+
+export const colors = Object.freeze({
+    /** #0078D4 */
+    "exchange.primary": "var(--token-032cc032-743f-4fe9-b7f1-eed9613c0ece, rgb(0, 120, 212))",
+    /** #FFFFFF */
+    "white": "var(--token-1a5922a1-947b-484e-af01-6f0b94430143, rgb(255, 255, 255))",
+})
