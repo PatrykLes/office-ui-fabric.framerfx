daa446389efa5bf5cfe7b18146f35b7b590995b0
Fernando Hurtado <fernandohur@gmail.com>
2019-09-04 11:23:41 +0200

    Cleaned up Facepile

--- code/items-and-lists/Facepile.tsx
+++ code/items-and-lists/Facepile.tsx
@@ -2,14 +2,16 @@ import * as React from "react";
 import * as System from "office-ui-fabric-react";
 import { ControlType, PropertyControls, addPropertyControls } from "framer";
 import { withHOC } from "../utils/withHOC";
+import { centeredChildStyles } from "../utils/CenteredWrapper";
 
-const style: React.CSSProperties = {
-  width: "100%",
-  height: "100%"
-};
-
-const InnerFacepile: React.SFC = props => {
-  return <System.Facepile {...props} style={style} />;
+const InnerFacepile: React.SFC<any> = ({ personas, ...props }) => {
+  return (
+    <System.Facepile
+      personas={personas.map(url => ({ imageUrl: url }))}
+      {...props}
+      styles={{ root: centeredChildStyles }}
+    />
+  );
 };
 
 export const Facepile = withHOC(InnerFacepile);
@@ -20,21 +22,49 @@ Facepile.defaultProps = {
 };
 
 addPropertyControls(Facepile, {
-  className: { title: "ClassName", defaultValue: "", type: ControlType.String },
   maxDisplayablePersonas: {
     title: "MaxDisplayablePersonas",
-    type: ControlType.Number
+    type: ControlType.Number,
+    defaultValue: 3,
+    min: 1,
+    max: 50,
+    displayStepper: true
   },
-  personaSize: { title: "PersonaSize", type: ControlType.Number },
-  ariaDescription: {
-    title: "AriaDescription",
-    defaultValue: "",
-    type: ControlType.String
+  personaSize: {
+    title: "PersonaSize",
+    type: ControlType.Number,
+    min: 1,
+    max: 6,
+    defaultValue: 10,
+    displayStepper: true
+  },
+  personas: {
+    type: ControlType.Array,
+    propertyControl: {
+      type: ControlType.String,
+      defaultValue: "https://api.adorable.io/avatars/285/0@adorable.io.png"
+    },
+    defaultValue: [
+      "https://api.adorable.io/avatars/285/0@adorable.io.png",
+      "https://api.adorable.io/avatars/285/1@adorable.io.png",
+      "https://api.adorable.io/avatars/285/2@adorable.io.png",
+      "https://api.adorable.io/avatars/285/3@adorable.io.png",
+      "https://api.adorable.io/avatars/285/4@adorable.io.png",
+      "https://api.adorable.io/avatars/285/5@adorable.io.png",
+      "https://api.adorable.io/avatars/285/6@adorable.io.png"
+    ]
   },
   showAddButton: {
     title: "ShowAddButton",
-    defaultValue: false,
+    defaultValue: true,
     type: ControlType.Boolean
   },
-  overflowButtonType: { title: "OverflowButtonType", type: ControlType.Number }
+  overflowButtonType: {
+    title: "OverflowButtonType",
+    type: ControlType.Number,
+    min: 1,
+    max: 3,
+    displayStepper: true,
+    defaultValue: 1
+  }
 });
