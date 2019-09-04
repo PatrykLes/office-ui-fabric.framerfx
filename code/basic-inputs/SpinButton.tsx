d488c89123f8564ce3f9c381d1dc2b9e5b94c726
iKettles <iain@framer.com>
2019-09-04 13:41:22 +0200

    Implement SpinButton

--- code/basic-inputs/SpinButton.tsx
+++ code/basic-inputs/SpinButton.tsx
@@ -2,65 +2,61 @@ import * as React from "react";
 import * as System from "office-ui-fabric-react";
 import { ControlType, PropertyControls, addPropertyControls } from "framer";
 import { withHOC } from "../utils/withHOC";
-
-const style: React.CSSProperties = {
-  width: "100%",
-  height: "100%"
-};
+import { Position } from "office-ui-fabric-react/lib/utilities/positioning";
 
 const InnerSpinButton: React.SFC = props => {
-  return <System.SpinButton {...props} style={style} />;
+  return <System.SpinButton {...props} />;
 };
 
 export const SpinButton = withHOC(InnerSpinButton);
 
 SpinButton.defaultProps = {
-  width: 150,
-  height: 50
+  width: 211,
+  height: 34
 };
 
+const positionKeys = Object.keys(Position).filter((key: string | number) =>
+  isNaN(key as any)
+);
+
 addPropertyControls(SpinButton, {
+  title: { title: "Title", defaultValue: "Title", type: ControlType.String },
+  label: { title: "Label", defaultValue: "Label", type: ControlType.String },
+  value: { title: "Value", defaultValue: "1", type: ControlType.String },
+  min: {
+    title: "Min",
+    type: ControlType.Number,
+    defaultValue: 0,
+    displayStepper: true
+  },
+  max: {
+    title: "Max",
+    type: ControlType.Number,
+    defaultValue: 10,
+    displayStepper: true
+  },
+  step: {
+    title: "Step",
+    type: ControlType.Number,
+    defaultValue: 1,
+    displayStepper: true
+  },
   defaultValue: {
     title: "DefaultValue",
     defaultValue: "",
     type: ControlType.String
   },
-  value: { title: "Value", defaultValue: "", type: ControlType.String },
-  min: { title: "Min", type: ControlType.Number },
-  max: { title: "Max", type: ControlType.Number },
-  step: { title: "Step", type: ControlType.Number },
-  ariaLabel: { title: "AriaLabel", defaultValue: "", type: ControlType.String },
-  ariaDescribedBy: {
-    title: "AriaDescribedBy",
-    defaultValue: "",
-    type: ControlType.String
-  },
-  title: { title: "Title", defaultValue: "", type: ControlType.String },
   disabled: {
     title: "Disabled",
     defaultValue: false,
     type: ControlType.Boolean
   },
-  className: { title: "ClassName", defaultValue: "", type: ControlType.String },
-  label: { title: "Label", defaultValue: "", type: ControlType.String },
-  labelPosition: { title: "LabelPosition", type: ControlType.Number },
-  incrementButtonAriaLabel: {
-    title: "IncrementButtonAriaLabel",
-    defaultValue: "",
-    type: ControlType.String
+  labelPosition: {
+    title: "LabelPosition",
+    type: ControlType.Enum,
+    defaultValue: Position.start,
+    options: positionKeys.map(key => String(Position[key])),
+    optionTitles: positionKeys
   },
-  decrementButtonAriaLabel: {
-    title: "DecrementButtonAriaLabel",
-    defaultValue: "",
-    type: ControlType.String
-  },
-  precision: { title: "Precision", type: ControlType.Number },
-  ariaPositionInSet: { title: "AriaPositionInSet", type: ControlType.Number },
-  ariaSetSize: { title: "AriaSetSize", type: ControlType.Number },
-  ariaValueNow: { title: "AriaValueNow", type: ControlType.Number },
-  ariaValueText: {
-    title: "AriaValueText",
-    defaultValue: "",
-    type: ControlType.String
-  }
+  precision: { title: "Precision", type: ControlType.Number }
 });
