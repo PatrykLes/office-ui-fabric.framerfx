41b4998c3a617072c046722d38fa9dee259c78a4
Fernando Hurtado <fernandohur@gmail.com>
2019-09-04 16:43:21 +0200

    Added office screen template

--- code/canvas.tsx
+++ code/canvas.tsx
@@ -1,6 +1,7 @@
 // WARNING: this file is auto generated, any changes will be lost
 import { createDesignComponent, CanvasStore } from "framer"
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const Office_Screen_Template = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_HeaGnIgCV", {}, 1440,900);
 export const Registration_Screen_Template = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_igRs33atD", {}, 1440,900);
 
 export const colors = Object.freeze({
