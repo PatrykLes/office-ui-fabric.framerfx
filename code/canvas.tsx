67eaa39d271e063dd5b0f738627f15db8b121d0a
Fernando Hurtado <fernandohur@gmail.com>
2019-09-04 15:10:26 +0200

    Added Office 365 example

--- code/canvas.tsx
+++ code/canvas.tsx
@@ -1,10 +1,21 @@
 // WARNING: this file is auto generated, any changes will be lost
 import { createDesignComponent, CanvasStore } from "framer"
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const Registration_Screen_Template = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_igRs33atD", {}, 1440,900);
 
 export const colors = Object.freeze({
     /** #0078D4 */
     "exchange.primary": "var(--token-032cc032-743f-4fe9-b7f1-eed9613c0ece, rgb(0, 120, 212))",
     /** #FFFFFF */
     "white": "var(--token-1a5922a1-947b-484e-af01-6f0b94430143, rgb(255, 255, 255))",
+    /** #F5F3F1 */
+    "office.navbar": "var(--token-fab702b4-6715-409d-9973-0e546b4c87a1, rgb(245, 243, 241))",
+    /** #D53A00 */
+    "office.primary": "var(--token-395710fc-2bb2-4623-bb54-4ecf586d26a5, rgb(213, 58, 0))",
+    /** #252423 */
+    "office.navbar.text": "var(--token-38b23bda-30b5-42f8-934b-f3fd2f51deaa, rgb(37, 36, 35))",
+    /** #F3F2F1 */
+    "office.background": "var(--token-ecc8e004-e8ba-482e-a94b-36914355c3d5, rgb(243, 242, 241))",
+    /** #0072C6 */
+    "office.link": "var(--token-c87338da-18c1-4873-b9dd-43e15b0c9775, rgb(0, 114, 198))",
 })
